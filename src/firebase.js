7e797a0e83d501c981e6a14ba28c66dbc4b3636d
Dhimas <satriyowibowo559@gmail.com>
2025-05-14 02:46:49 +0700

    add env

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAv_32K4T5SZV8raLDcQnsjk97dXDM0GIY",
-  authDomain: "wedding-invitation-dhims.firebaseapp.com",
-  projectId: "wedding-invitation-dhims",
-  storageBucket: "wedding-invitation-dhims.firebasestorage.app",
-  messagingSenderId: "294445007433",
-  appId: "1:294445007433:web:4d5d992ae8711f72f1f598"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID
 };
 
 // Inisialisasi Firebase
